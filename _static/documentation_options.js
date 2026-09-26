fda9a6be8c9d72f4d1cdb0bbfcbb40dca6287d57
ahfoss <ahfoss@users.noreply.github.com>
2026-09-26 05:04:57 +0000

    docs: deploy documentation [ci skip] a37b7d6554ee66d8076031554e73aad5de57979c

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.0.dev21+gb78007720',
+    VERSION: '0.1.0.dev23+ga37b7d655',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
